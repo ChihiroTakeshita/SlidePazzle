6a134f65af612109729c9faf8d5a594b0d308502
ChihiroTakeshita <83819925+ChihiroTakeshita@users.noreply.github.com>
2021-08-22 15:13:37 +0900

    開始時ブロックがランダム生成されるようになった

---
+++ Assets/Scripts/CreateBlocks.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBlocks : MonoBehaviour
+{
+    [SerializeField] GameObject[] blocks;
+
+    int width = 5;
+    int height = 5;
+    float blockSize = 1.28f;
+
+    GameObject[,] blockArray = new GameObject[5, 5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                int r = Random.Range(0, 4);
+                var block = Instantiate(blocks[r]);
+                block.transform.position = new Vector3(blockSize * i, blockSize * j);
+                blockArray[i, j] = block;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーン遷移用
+/// </summary>
+
+public class SceneManager : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    /// <summary>
+    /// ゲーム画面を読み込みます。
+    /// </summary>
+    public void LoadGame()
+    {
+
+    }
+
+    /// <summary>
+    /// リザルト画面を読み込みます。
+    /// </summary>
+    public void LoadResult()
+    {
+
+    }
+
+    /// <summary>
+    /// タイトル画面を読み込みます。
+    /// </summary>
+    public void LoadTitle()
+    {
+
+    }
+}
